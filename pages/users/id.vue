8a440eca9f54e816cf1a9b31ee2746f8a29667ef
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-11-17 13:32:38 +0700

    Router Pages

---
+++ pages/users/id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Users Detail : {{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+    }
+  },
+}
+</script>
